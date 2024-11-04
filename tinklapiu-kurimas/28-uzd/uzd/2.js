c4aac7e68f3ceb362f6c6a4f79f70ab5a73599d1
D2JP <d2jp@TECHIN.INTERNAL>
2024-11-04 10:49:28 +0200

    28-uzd, 2 JS uzd

---
+++ tinklapiu-kurimas/28-uzd/uzd/2.js
@@ -0,0 +1,5 @@
+const zuvuKiekis = parseInt(prompt("Kiek žuvų gyvena akvariume?"))
+const zuvuKiekvienaDiena = parseInt(prompt("Kiek žuvų į akvariumą įdedama kiekvieną dieną?"))
+const dienuPraejo = parseInt(prompt("Kiek dienų praėjo?"))
+
+console.log(`Po ${dienuPraejo} dienų akvariume gyvens ${zuvuKiekis+(zuvuKiekvienaDiena*dienuPraejo)} žuvų.`)
