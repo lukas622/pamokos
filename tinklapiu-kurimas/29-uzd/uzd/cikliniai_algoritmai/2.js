79f305338ec701e42717b7eee47d99a7d6b2d240
lukas622 <bsds356@proton.me>
2024-11-05 12:39:40 +0400

    29-uzd, add 2 JS uzd (cikliniai)

---
+++ tinklapiu-kurimas/29-uzd/uzd/cikliniai_algoritmai/2.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let stepsAmount = parseInt(prompt("Įveskite žingsnių kiekį iki mokyklos"));
+
+let claps = 0;
+let clicks = 0;
+
+for (stepsAmount; stepsAmount > 0; stepsAmount--) {
+  if (stepsAmount % 10 === 0) {
+    claps++;
+  } else if (stepsAmount % 10 === 5) {
+    clicks++;
+  }
+}
+
+console.log(`Suplojimų bus ${claps}`);
+console.log(`Spragtelėjimų bus ${clicks}`);
